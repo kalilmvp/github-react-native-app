9fd43ebccc1364fe2f61e5c62bd1cdf5c9cd3173
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2019-11-24 16:50:42 +0100

    Initial configuration for react native application

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+// import { Container } from './styles';
+
+export default function Main() {
+  return <Container />;
+}
+
+Main.navigationOptions = {
+  title: 'Users',
+};
